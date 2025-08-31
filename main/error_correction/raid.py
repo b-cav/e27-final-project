5a36cdd2cf5b41472ffe356460a4969e32a56a0d
b-cav <cavben38@gmail.com>
2025-08-30 22:17:39 -0400

    raid working

--- main/error_correction/raid.py
+++ main/error_correction/raid.py
@@ -11,8 +11,6 @@ RAID 5-style P packet (XOR parity) protection/recovery
 # Every s packets (stripe) add a parity packet
 # Return list of ordered data/parity packets
 def RAID_protect(data_packets, n = 16, s = 4) :
-    # Add one parity packet after every 4 data packets
-
     out_packets = []
 
     i = 0
@@ -35,13 +33,47 @@ def RAID_protect(data_packets, n = 16, s = 4) :
     print(f"Detected {i} data packets")
     return out_packets
 
+# Given a stream of Hamming-corrected data/P packets
+# Recover lost packets and clean out P packets
+def RAID_remove(data_packets, n = 16, s = 4) :
+    out_packets = []
+
+    i = 0
+    while i < len(data_packets) :
+        mult_err_loc = -1
+        unrecoverable = 0
+        stripe = data_packets[i:i+s+1]
+        print(f"STRIPE I'm removing: {stripe}")
+        # Check for multi-bit errors
+        # Make sure only one has happened in the stripe
+        for j in range(len(stripe)) :
+            repaired_16_bit, mult_err = multi_err_detect(stripe[j])
+            if mult_err :
+                if mult_err_loc == -1 :
+                    mult_err_loc = j
+                else :
+                    unrecoverable = 1
+            stripe[j] = repaired_16_bit
+
+        if not unrecoverable :
+            out_packets.extend(RAID_recover(stripe, mult_err_loc))
+        else :
+            out_packets.extend(stripe[:-1])
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            print("UNRECOVERABLE MULTI-BIT ERROR")
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+        i += (len(stripe))
+
+    return(out_packets)
+
 # Given a stripe of s data packets and 1 parity packet
 # and corrupted packet index i recover packet at index i
 # if necessary, return just data packets
 def RAID_recover(stripe, i, n = 16, s = 4) :
     # i is multi-error packet location.
-    # If i = s (0-indexed) then no error or error only in the parity packet
-    if i != s :
+    # If i = -1 then no error
+    if i != -1 :
         uncorrupted = stripe[:i] + stripe[i+1:]
 
         recovered = ""
@@ -55,10 +87,56 @@ def RAID_recover(stripe, i, n = 16, s = 4) :
     else :
         return(stripe[:-1])
 
+# Detect multi-bit errors, correct single bit errors, but don't shorten
+# Somewhat redundant but wanted black-box link to Hamming decoding
+def multi_err_detect(packet, n = 16) :
+    multi_err = False
+    p_bits = [0, 1, 2, 4, 8]
+
+    if len(packet) != n :
+        raise ValueError("Packet length not 16")
+
+    expanded = []
+    for i in range(n) :
+        expanded.append(packet[i])
+
+    # Norm parity bits to locate source of error
+    error_loc = 0
+    for p in p_bits[1:] : # ignore extension bit
+        par = 0
+        for i in range(1, 16) :
+            if i & p and expanded[i] == "1" :
+                par ^= 1
+        if par == 1 :
+            error_loc += p
+
+    # p0 parity bit to check for double error
+    ext_par = 0
+    for i in range(1, 16) :
+        if expanded[i] == "1" :
+            ext_par ^= 1
+
+    # Check if extension parity bit doesnt match
+    if ext_par != int(expanded[0]) :
+        # 1 error, fix it
+        expanded[error_loc] = str(int(expanded[error_loc])^1)
+    elif error_loc == 0 :
+        # No errors, pass through
+        pass
+    else :
+        print("MULTI-BIT ERROR")
+        print("ATTEMPTING TO RECOVER...")
+        multi_err = True
+
+    return("".join(expanded), multi_err)
+
 if __name__ == "__main__" :
     packets = ["0"*16, "1"*16, "01"*8, "10"*8]
 
     print(f"Packets: {packets}")
     protected = RAID_protect(packets)
+    print(f"Protected: {protected}")
     for j in range(5) :
         print(RAID_recover(protected, j))
+
+    print(f"RAID removed: {RAID_remove(protected)}")
--- main/main.py
+++ main/main.py
@@ -7,6 +7,7 @@ Group - Ben Sheppard, Joshua Johnson, Ben Cavanagh
 """
 from huffman import final_compression as fc
 from error_correction import hamming as hm
+from error_correction import raid
 
 def console(opt_codebook, bigram_list, opt_tree) :
     message = input("ENTER MESSAGE: ")
@@ -30,29 +31,37 @@ def console(opt_codebook, bigram_list, opt_tree) :
     # ************************************************
 
     # ------------------------------------------------
-    # 2) Encode Message:
+    # 2) Encode message:
     compressed = fc.encode_message(fc.replace_bigrams(message, bigram_list), opt_codebook)
     # pass in a list of symbols to encode with bigram codebook
 
     # ------------------------------------------------
-    # 3) Packetize, Add Parity Bits:
-    protected = hm.EHC_16_11_encode(compressed) # Splits data into 11 bit packets, pads last one
+    # 3) Packetize, add parity bits:
+    encoded = hm.EHC_16_11_encode(compressed) # Splits data into 11 bit packets, pads last one
 
     # ------------------------------------------------
-    # 4) Transmit Packets:
+    # 4) Add RAID P packets:
+    protected = raid.RAID_protect(encoded)
+
+    # ------------------------------------------------
+    # 5) Transmit packets:
     received_packets = hm.noisy_channel(protected)
 
     # ------------------------------------------------
-    # 5) Error Correct Received Packets:
-    corrected = hm.EHC_16_11_decode(received_packets)
+    # 6) Recover multi-bit packets, remove RAID P packets
+    raid_recovered = raid.RAID_remove(received_packets)
 
     # ------------------------------------------------
-    # 6) Remove Padding:
-    cleaned = hm.remove_padding(corrected)
+    # 7) Clean Hamming parity bits
+    cleaned = hm.EHC_16_11_clean(raid_recovered)
 
     # ------------------------------------------------
-    # 7) Decode:
-    decoded = fc.decode_message(cleaned, opt_tree)
+    # 8) Remove padding:
+    unpadded = hm.remove_padding(cleaned)
+
+    # ------------------------------------------------
+    # 9) Decode:
+    decoded = fc.decode_message(unpadded, opt_tree)
     print(f"PROTECTED MESSAGE: {decoded}")
    
  
